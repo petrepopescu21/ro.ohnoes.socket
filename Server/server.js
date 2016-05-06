5d8715b5d88f9752a84374afe060e54b8e2c45e3
Petre <petrepopescu21@gmail.com>
2016-05-06 11:27:38 +0300

    Added time

--- Server/server.js
+++ Server/server.js
@@ -2,13 +2,16 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
-app.use(express.static(__dirname + '/app'));
+
+//app.use(express.static(__dirname + '/app'));
+
 app.get('/', function(req, res,next) {  
-    res.send('Root!')
+    res.send('Root!');
 });
 
-app.get('/test', function(req, res,next) {  
-    res.send('Hello!');
+app.get('/cp', function(req,res,net) {
+    io.emit('time',{time:new Date().toJSON()});
+    res.send('Time sent.');
 });
 
 server.listen(process.env.PORT || 4200);  
